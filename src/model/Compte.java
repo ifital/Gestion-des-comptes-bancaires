2a90ab9584d84610153cf7c572da90241329ef6b
Latifi777 <abdelalilatifi3@gmail.com>
2025-09-16 16:49:48 +0100

    generate absract classes first

---
+++ src/model/Compte.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Compte {
+    protected String code;
+    protected double solde;
+    protected List<Operation> listeOperations;
+
+    public Compte(String code) {
+        this.code = code;
+        this.solde = 0.0;
+        this.listeOperations = new ArrayList<>();
+    }
+
+    public abstract boolean retirer(double montant, String destination);
+    public abstract double calculerInteret();
+    public abstract void afficherDetails();
+
+    public String getCode() {return code;}
+    public double getSolde() {return solde;}
+    public List<Operation> getListeOperations() {return listeOperations;}
+
+    public void ajouterOperation(Operation operation){
+        this.listeOperations.add(operation);
+    }
+}
