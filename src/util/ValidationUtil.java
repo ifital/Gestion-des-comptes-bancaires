ad54654277ad51a529cced42e1d7df864cf28322
Latifi777 <abdelalilatifi3@gmail.com>
2025-09-17 16:01:47 +0100

    validation of account information with ValidationUtil classe

---
+++ src/util/ValidationUtil.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.util.Stack;
+import java.util.regex.Pattern;
+
+public class ValidationUtil {
+
+    private static final Pattern CODE_Pattern = Pattern.compile("CPT-\\d{5}");
+
+    public static boolean ValiderCodeCompte(String code){
+        return code != null && CODE_Pattern.matcher(code).matches();
+    }
+
+    public static boolean ValiderMontatPositif(double montant){
+        return montant > 0 ;
+    }
+
+    public static boolean ValiderChaineNonVide(String chaine){
+            return chaine != null && !chaine.trim().isEmpty();
+    }
+
+    public  static String GenererCodeCompte(int numero){
+        return String.format("CPT-05d" + numero);
+    }
+}
